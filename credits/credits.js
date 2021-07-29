92673e6d697564e37f56bc6e53c0d7729e778a32
CoolAbhi1290 <43380238+CoolAbhi1290@users.noreply.github.com>
2021-07-29 13:22:26 -0500

    laid out infrastructure for credits page

---
+++ credits/credits.js
@@ -0,0 +1,3 @@
+window.addEventListener("load", async function () {
+    var contributors = fetch("https://api.github.com/repos/BedrockLauncher/BedrockLauncher/contributors?per_page=2147483647");
+});
