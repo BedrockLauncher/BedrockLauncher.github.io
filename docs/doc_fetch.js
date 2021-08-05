2b63f139fd80dbdbf3716bca9d17a94bea9dad13
CoolAbhi1290 <43380238+CoolAbhi1290@users.noreply.github.com>
2021-08-05 16:15:48 -0500

    added content to docs

--- docs/doc_fetch.js
+++ docs/doc_fetch.js
@@ -1,5 +1,16 @@
 window.addEventListener("load", async function () {
+    
+    
     if (!DOC_NAME) return;
+
+    var target = document.getElementById("target");
     
     var fullPath = `https://raw.githubusercontent.com/CoolAbhi1290/BedrockLauncher/master/docs/${DOC_NAME}.md`;
+    var doc = await fetch(fullPath);
+    var docText = await doc.text();
+    
+    var markdownConverter = new showdown.Converter();
+    var docHtml = markdownConverter.makeHtml(docText);
+    
+    target.innerHTML = docHtml;
 })
