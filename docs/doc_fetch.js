6fb7ea278a5844f16e259339028ea3e94fc725b4
CoolAbhi1290 <43380238+CoolAbhi1290@users.noreply.github.com>
2021-08-05 15:47:19 -0500

    added infrastructure for docs

---
+++ docs/doc_fetch.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", async function () {
+    if (!DOC_NAME) return;
+    
+    var fullPath = `https://raw.githubusercontent.com/CoolAbhi1290/BedrockLauncher/master/docs/${DOC_NAME}.md`;
+})
