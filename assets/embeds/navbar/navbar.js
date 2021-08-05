0c9a2a872e99c52760148b5bd53ac474dd999091
CoolAbhi1290 <43380238+CoolAbhi1290@users.noreply.github.com>
2021-08-05 15:15:32 -0500

    added cancel button

--- assets/embeds/navbar/navbar.js
+++ assets/embeds/navbar/navbar.js
@@ -1,4 +1,4 @@
-var embed, background, transitionDuration, expandedHamburgur, logoImg;
+var embed, background, transitionDuration, transitionDurationSemiSlow, expandedHamburgur, logoImg, hamburgur, hamburgurIcon;
 
 var isExpanded = false;
 
@@ -10,7 +10,11 @@ async function expand() {
     background.classList.add("visible");
     expandedHamburgur.classList.add("expanded");
     logoImg.classList.remove("shrink");
+    hamburgurIcon.classList.add("expanded");
     embed.classList.add("expanded");
+
+    await sleep(transitionDurationSemiSlow * 1000 / 2);
+    hamburgurIcon.src = "/assets/images/icons/cancel.svg";
 
     isExpanded = true;
 }
@@ -19,17 +23,26 @@ async function shrink() {
     background.classList.remove("visible");
     expandedHamburgur.classList.remove("expanded");
     logoImg.classList.add("shrink");
-    await sleep(transitionDuration * 1000);
+    hamburgurIcon.classList.remove("expanded");
+
+    await sleep(transitionDurationSemiSlow * 1000 / 2);
+    hamburgurIcon.src = "/assets/images/icons/hamburger.svg";
+
+    await sleep(((transitionDurationSemiSlow / 2) - transitionDuration) * 1000);
     embed.classList.remove("expanded");
 
     isExpanded = false;
 }
 
 window.addEventListener("load", async function () {
-    var transitionDurationString = getComputedStyle(document.documentElement).getPropertyValue("--transition-duration-general");
+    var computedStyle = getComputedStyle(document.documentElement)
+    var transitionDurationString = computedStyle.getPropertyValue("--transition-duration-general");
+    var transitionDurationSemiSlowString = computedStyle.getPropertyValue("--transition-duration-semi-slow");
     transitionDuration = Number(transitionDurationString.slice(0, transitionDurationString.length - 1));
+    transitionDurationSemiSlow = Number(transitionDurationSemiSlowString.slice(0, transitionDurationSemiSlowString.length - 1));
 
-    var hamburgur = document.getElementById("hamburger");
+    hamburgur = document.getElementById("hamburger");
+    hamburgurIcon = document.getElementById("hamburger-icon");
     background = document.getElementById("background");
     embed = parent.window.document.getElementById("nav-bar");
     expandedHamburgur = document.getElementById("expanded-hamburgur");
