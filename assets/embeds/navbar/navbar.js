d00d7b54d114c162b277e7c6382e9f72a12f0522
CoolAbhi1290 <43380238+CoolAbhi1290@users.noreply.github.com>
2021-08-06 16:15:29 -0500

    added search button - hamgburgur interactions

--- assets/embeds/navbar/navbar.js
+++ assets/embeds/navbar/navbar.js
@@ -1,44 +1,66 @@
-var embed, background, transitionDuration, expandedHamburgur, logoImg, hamburgur, hamburgurIcon, search;
-
-var isExpanded = false;
+var embed, background, transitionDuration, expandedHamburgur, logoImg, hamburgur, hamburgurIcon, search, expandedSearch, searchIcon;
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function expand() {
+async function darkenAndExpand() {
     background.classList.add("visible");
-    expandedHamburgur.classList.add("expanded");
-    logoImg.classList.remove("shrink");
-    hamburgurIcon.classList.add("expanded");
     embed.classList.add("expanded");
+    logoImg.classList.remove("shrink");
+}
+
+async function unDarkenAndShrink() {
+    background.classList.remove("visible");
+    logoImg.classList.add("shrink");
+
+    await sleep(transitionDuration * 1000 / 2);
+    embed.classList.remove("expanded");
+}
+
+async function expandHamburgur() {
+    shrinkSearch();
+    
+    expandedHamburgur.classList.add("expanded");
+    hamburgurIcon.classList.add("expanded");
 
     await sleep(transitionDuration * 1000 / 2);
     hamburgurIcon.src = "/assets/images/icons/cancel.svg";
-
-    isExpanded = true;
 }
 
-async function shrink() {
-    background.classList.remove("visible");
+async function shrinkHamburgur() {
     expandedHamburgur.classList.remove("expanded");
-    logoImg.classList.add("shrink");
     hamburgurIcon.classList.remove("expanded");
 
     await sleep(transitionDuration * 1000 / 2);
     hamburgurIcon.src = "/assets/images/icons/hamburger.svg";
-    embed.classList.remove("expanded");
-
-    isExpanded = false;
 }
 
-async function fetchQueryResults (query) {
+async function expandSearch() {
+    shrinkHamburgur();
+    
+    expandedSearch.classList.add("expanded");
+    searchIcon.classList.add("expanded");
+
+    await sleep(transitionDuration * 1000 / 2);
+    searchIcon.src = "/assets/images/icons/cancel.svg";
+}
+
+async function shrinkSearch() {
+    expandedSearch.classList.remove("expanded");
+    searchIcon.classList.remove("expanded");
+
+    await sleep(transitionDuration * 1000 / 2);
+    searchIcon.src = "/assets/images/icons/search.svg";
+}
+
+async function fetchQueryResults(query) {
     var key = "AIzaSyDao5Lwp0lQM9VF8OVf3zzSR92M5U_XLPA";
     var cx = "5f283ba92be0b61e1";
-    
+
     var fetchResult = await fetch(`https://www.googleapis.com/customsearch/v1?key=${key}&cx=${cx}&q=${query}`);
     var fetchJson = await fetchResult.json();
-    
+
     return fetchJson.items;
 }
 
@@ -52,15 +74,32 @@ window.addEventListener("load", async function () {
     background = document.getElementById("background");
     embed = parent.window.document.getElementById("nav-bar");
     expandedHamburgur = document.getElementById("expanded-hamburgur");
+    expandedSearch = document.getElementById("expanded-search");
     logoImg = document.getElementById("logo-img");
     search = document.getElementById("search");
+    searchIcon = document.getElementById("search-icon");
 
     hamburgur.addEventListener("click", function () {
-        if (isExpanded) {
-            shrink();
+        if (expandedHamburgur.classList.contains("expanded")) {
+            unDarkenAndShrink();
+            shrinkHamburgur();
         } else {
-            expand();
+            darkenAndExpand();
+            expandHamburgur();
         }
     })
-    background.addEventListener("click", shrink);
+    search.addEventListener("click", function () {
+        if (expandedSearch.classList.contains("expanded")) {
+            unDarkenAndShrink();
+            shrinkSearch();
+        } else {
+            darkenAndExpand();
+            expandSearch();
+        }
+    })
+    background.addEventListener("click", function () {
+        unDarkenAndShrink();
+        shrinkHamburgur();
+        shrinkSearch();
+    });
 })
