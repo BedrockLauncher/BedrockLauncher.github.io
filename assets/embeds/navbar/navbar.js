017904368dd519928aa4ad383dda2afa88f1ab39
CoolAbhi1290 <43380238+CoolAbhi1290@users.noreply.github.com>
2021-08-07 15:33:16 -0500

    added search limit fallback

--- assets/embeds/navbar/navbar.js
+++ assets/embeds/navbar/navbar.js
@@ -1,4 +1,4 @@
-var embed, background, transitionDuration, expandedHamburgur, logoImg, hamburgur, hamburgurIcon, search, expandedSearch, searchIcon, results;
+var embed, background, transitionDuration, expandedHamburgur, logoImg, hamburgur, hamburgurIcon, search, expandedSearch, searchIcon, results, searchBox;
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -20,7 +20,7 @@ async function unDarkenAndShrink() {
 
 async function expandHamburgur() {
     shrinkSearch();
-    
+
     expandedHamburgur.classList.add("expanded");
     hamburgurIcon.classList.add("expanded");
 
@@ -38,7 +38,7 @@ async function shrinkHamburgur() {
 
 async function expandSearch() {
     shrinkHamburgur();
-    
+
     expandedSearch.classList.add("expanded");
     searchIcon.classList.add("expanded");
 
@@ -54,39 +54,66 @@ async function shrinkSearch() {
     searchIcon.src = "/assets/images/icons/search.svg";
 }
 
-async function fetchQueryResults(query) {
+async function fetchQueryResults() {
+    var query = searchBox.value.trim();
+
+    if (query.length == 0) return;
+
     var key = "AIzaSyDao5Lwp0lQM9VF8OVf3zzSR92M5U_XLPA";
     var cx = "5f283ba92be0b61e1";
 
     var fetchResult = await fetch(`https://www.googleapis.com/customsearch/v1?key=${key}&cx=${cx}&q=${query}`);
-    var resultsJson = (await fetchResult.json()).items;
-    
+    var fetchJson = await fetchResult.json();
+
+    if (fetchJson.error) {
+        if (confirm("Daily search limit has been exceeded, redirect to default search page?")) {
+            location.href = `https://cse.google.com/cse?cx=5f283ba92be0b61e1&q=${query}`;
+        }
+
+        return;
+    };
+
+    var resultsJson = fetchJson.items;
+
     results.innerHTML = "";
-    
-    for (resultIndex in resultsJson) {
-        var result = resultsJson[resultIndex];
-        
-        var resultElement = document.createElement("div");
-        var hr = document.createElement("hr");
-        var resultTitle = document.createElement("p");
-        var resultTitleLink = document.createElement("a");
-        var resultText = document.createElement("p");
-        
-        resultElement.classList.add("result");
-        resultTitle.classList.add("result-title");
-        resultText.classList.add("result-text");
-        
-        resultTitleLink.href = result.link;
-        resultTitleLink.innerText = result.title;
-        resultTitleLink.target = "_parent";
-        resultText.innerText = result.snippet;
-        
-        resultTitle.appendChild(resultTitleLink);
-        resultElement.appendChild(resultTitle);
-        resultElement.appendChild(resultText);
-        
-        results.appendChild(resultElement);
-        results.appendChild(hr);
+
+    if (resultsJson) {
+        for (resultIndex in resultsJson) {
+            var result = resultsJson[resultIndex];
+
+            var resultElement = document.createElement("div");
+            var hr = document.createElement("hr");
+            var resultTitle = document.createElement("p");
+            var resultTitleLink = document.createElement("a");
+            var resultText = document.createElement("p");
+
+            resultElement.classList.add("result");
+            resultTitle.classList.add("result-title");
+            resultText.classList.add("result-text");
+
+            resultTitleLink.href = result.link;
+            resultTitleLink.innerText = result.title;
+            resultTitleLink.target = "_parent";
+            resultText.innerText = result.snippet;
+
+            resultTitle.appendChild(resultTitleLink);
+            resultElement.appendChild(resultTitle);
+            resultElement.appendChild(resultText);
+
+            results.appendChild(resultElement);
+            results.appendChild(hr);
+        }
+    } else {
+        var p = document.createElement("p");
+        var p2 = document.createElement("p");
+
+        p.classList.add("center");
+        p.innerHTML = `No results found for <b>${query}</b>.`
+        p2.classList.add("center");
+        p2.innerHTML = "<i>Try rewording your search, or correcting your spelling.</i>"
+
+        results.appendChild(p);
+        results.appendChild(p2);
     }
 }
 
@@ -105,6 +132,7 @@ window.addEventListener("load", async function () {
     search = document.getElementById("search");
     searchIcon = document.getElementById("search-icon");
     results = document.getElementById("results");
+    searchBox = document.getElementById("search-box");
 
     hamburgur.addEventListener("click", function () {
         if (expandedHamburgur.classList.contains("expanded")) {
@@ -130,5 +158,7 @@ window.addEventListener("load", async function () {
         shrinkSearch();
     });
 
-fetchQueryResults("version");
+    searchBox.addEventListener("keyup", function (event) {
+        if (event.keyCode == 13) fetchQueryResults();
+    })
 })
